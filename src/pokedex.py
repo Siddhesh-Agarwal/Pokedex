7f1450582d1711eb3fd1dae244ec9e8b4ab153ae
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2021-07-23 14:40:49 +0000

    Updated pokedex.py

--- src/pokedex.py
+++ src/pokedex.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pokebase as pb
-import requests, json
+import requests
 
 st.title("Pokedex")
 st.write("Search the pokemon you are looking for and you will get the details.")
@@ -8,4 +8,15 @@ st.write("Search the pokemon you are looking for and you will get the details.")
 name = st.text_input("Search Pokemon here:")
 
 if st.button("Search..."):
-    pass
+    poke = pb.pokemon(name)
+    st.header("Information")
+    st.markdown(f"**Name**: {name.title()}")
+    st.markdown(f"**Type**:")
+    for slot, type_slot in enumerate(poke.types, start=1):
+        st.write(f"{slot}. {type_slot.type.name}")
+    st.markdown(f"**Height**: {poke.height}")
+    st.markdown(f"**Weight**: {poke.weight}")
+    st.markdown(f"**Base experience**: {poke.base_experience}")
+    st.markdown(f"**Abilities**:")
+    for sno, ability in enumerate(poke.abilities, start=1):
+        st.write(f"{sno}. {ability.ability.name.replace('-', ' ')}")
